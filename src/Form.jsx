c39ed656dcdba7123fabffd77de19b6ac16bd393
RosyPetrella <petrellamr@gmail.com>
2025-03-31 16:25:41 +0200

    Edit Form component to enhance layout and add input fields for Author, Title, Body, and Public status

--- src/Form.jsx
+++ src/Form.jsx
@@ -10,24 +10,69 @@ export default function Form() {
   return (
     <>
       <form className="container mt-4">
-        <div className="mb-2">
-          <label htmlFor=""></label>
-          <input type="text" name="" id="" />
+        <div className="row mb-3">
+          <label for="colFormLabel" class="col-sm-2 col-form-label">
+            Author
+          </label>
+          <div className="col-sm-10">
+            <input type="text" class="form-control" id="colFormLabel"></input>
+          </div>
         </div>
 
-        <div className="mb-2">
-          <label htmlFor=""></label>
-          <input type="text" name="" id="" />
+        <div className="row mb-3">
+          <label for="colFormLabel" class="col-sm-2 col-form-label">
+            Title
+          </label>
+          <div className="col-sm-10">
+            <input type="text" class="form-control" id="colFormLabel"></input>
+          </div>
         </div>
 
-        <div className="mb-2">
-          <label htmlFor=""></label>
-          <input type="text" name="" id="" />
+        <div className="row mb-3">
+          <label for="colFormLabel" class="col-sm-2 col-form-label">
+            Body
+          </label>
+          <div className="col-sm-10">
+            <input type="text" class="form-control" id="colFormLabel"></input>
+          </div>
         </div>
 
-        <div className="mb-2">
-          <label htmlFor=""></label>
-          <input type="checkbox" name="" id="" />
+        <div className="row mb-3">
+          <label for="colFormLabel" class="col-sm-2 col-form-label">
+            Public
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="form-check-input"
+              class="form-control"
+              id="colFormLabel"
+            ></input>
+          </div>
+        </div>
+
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          ></input>
+          <label class="form-check-label" for="flexRadioDefault1">
+            Default radio
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            checked
+          ></input>
+          <label class="form-check-label" for="flexRadioDefault2">
+            Default checked radio
+          </label>
         </div>
       </form>
     </>
